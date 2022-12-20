f36279abf5d69b959a4ac081f718e5ad89329ecf
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2022-12-20 14:48:47 +0100

    Attempted to fix non-declaration statement error

--- src/main.go
+++ src/main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-    "net/http"
-    "os"
-    "log"
+	"log"
+	"net/http"
+	"os"
 )
 
-fsmqLogLevel := os.Getenv("FSMQ_LOG_LEVEL")
+var fsmqLogLevel string = os.Getenv("FSMQ_LOG_LEVEL")
 
-func main () {
-    fsmqAppPort := os.Getenv("FSMQ_APP_PORT")
-    if fsmqAppPort == "" {
-        fsmqAppPort = "8080"
-    }
-    fsmqRoutePrefix := os.Getenv("FSMQ_ROUTE_PREFIX")
+func main() {
+	fsmqAppPort := os.Getenv("FSMQ_APP_PORT")
+	if fsmqAppPort == "" {
+		fsmqAppPort = "8080"
+	}
+	fsmqRoutePrefix := os.Getenv("FSMQ_ROUTE_PREFIX")
 
-    http.HandleFunc(fsmqRoutePrefix + "/token/get", GetToken)
-    http.HandleFunc(fsmqRoutePrefix + "/healthcheck", Healthcheck)
-    http.HandleFunc(fsmqRoutePrefix + "/queue/get-job", QueueEndpoint)
-    http.HandleFunc(fsmqRoutePrefix + "/queue/get-batch", QueueEndpoint)
-    http.HandleFunc(fsmqRoutePrefix + "/queue/get-job-payload", QueueEndpoint)
-    http.HandleFunc(fsmqRoutePrefix + "/queue/ack-job", QueueEndpoint)
-    http.HandleFunc(fsmqRoutePrefix + "/queue/lock-job", QueueEndpoint)
-    http.HandleFunc(fsmqRoutePrefix + "/queue/unlock-job", QueueEndpoint)
-    http.HandleFunc(fsmqRoutePrefix + "/queue/put-job", QueueEndpoint)
-    log.Fatalln(http.ListenAndServe(":" + fsmqAppPort, nil))
+	http.HandleFunc(fsmqRoutePrefix+"/token/get", GetToken)
+	http.HandleFunc(fsmqRoutePrefix+"/healthcheck", Healthcheck)
+	http.HandleFunc(fsmqRoutePrefix+"/queue/get-job", QueueEndpoint)
+	http.HandleFunc(fsmqRoutePrefix+"/queue/get-batch", QueueEndpoint)
+	http.HandleFunc(fsmqRoutePrefix+"/queue/get-job-payload", QueueEndpoint)
+	http.HandleFunc(fsmqRoutePrefix+"/queue/ack-job", QueueEndpoint)
+	http.HandleFunc(fsmqRoutePrefix+"/queue/lock-job", QueueEndpoint)
+	http.HandleFunc(fsmqRoutePrefix+"/queue/unlock-job", QueueEndpoint)
+	http.HandleFunc(fsmqRoutePrefix+"/queue/put-job", QueueEndpoint)
+	log.Fatalln(http.ListenAndServe(":"+fsmqAppPort, nil))
 }
