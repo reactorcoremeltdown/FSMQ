264cbb279a375f331dc827125fc5614efc80c1eb
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2022-12-20 14:50:12 +0100

    Fixed type mismatch

--- src/authtoken.go
+++ src/authtoken.go
@@ -1,76 +1,77 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "log"
-    "encoding/json"
-    "io/ioutil"
-    "os"
-    "strconv"
-    "github.com/satori/go.uuid"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 type User struct {
-    Name string
-    Key string
-    Producer bool
+	Name     string
+	Key      string
+	Producer bool
 }
 
 func GetToken(res http.ResponseWriter, req *http.Request) {
-    if req.Method != "POST" {
-        res.WriteHeader(503)
-        fmt.Fprint(res, "This endpoint processes POST requests only")
-    } else {
-        err := req.ParseForm()
-        if err != nil {
-            log.Println("Failed to parse form data: " + err.Error())
-        } else {
-            if fsmqLogLevel == 'debug' {
-                for key, value := range req.Form {
-                    log.Printf("%s = %s\n", key, value)
-                }
-            }
-        }
-        authtoken := req.Form.Get("token")
+	if req.Method != "POST" {
+		res.WriteHeader(503)
+		fmt.Fprint(res, "This endpoint processes POST requests only")
+	} else {
+		err := req.ParseForm()
+		if err != nil {
+			log.Println("Failed to parse form data: " + err.Error())
+		} else {
+			if fsmqLogLevel == "debug" {
+				for key, value := range req.Form {
+					log.Printf("%s = %s\n", key, value)
+				}
+			}
+		}
+		authtoken := req.Form.Get("token")
 
-        var bin []byte
-        if bin, err = ioutil.ReadFile(os.Getenv("FSMQ_ACL_FILE")); err != nil {
-            log.Fatalln("Can't read user database: " + err.Error())
-        }
+		var bin []byte
+		if bin, err = ioutil.ReadFile(os.Getenv("FSMQ_ACL_FILE")); err != nil {
+			log.Fatalln("Can't read user database: " + err.Error())
+		}
 
-        var users []User
-        err = json.Unmarshal(bin, &users)
-        if err != nil {
-            log.Fatalln("Can't read config file")
-        }
+		var users []User
+		err = json.Unmarshal(bin, &users)
+		if err != nil {
+			log.Fatalln("Can't read config file")
+		}
 
-        authSucceeded := false
-        for _, element := range users {
-            if element.Key == authtoken {
-                authSucceeded = true
-                tokenUUID := uuid.NewV4()
-                if err != nil {
-                    log.Fatalln("Cannot issue UUIDv4 token: " + err.Error())
-                }
-                tokenFile, err := os.Create(os.Getenv("FSMQ_TOKEN_POOL_PATH") + "/token-" + tokenUUID.String())
-                if err != nil {
-                    log.Fatalln("Cannot create token file: " + err.Error())
-                }
+		authSucceeded := false
+		for _, element := range users {
+			if element.Key == authtoken {
+				authSucceeded = true
+				tokenUUID := uuid.NewV4()
+				if err != nil {
+					log.Fatalln("Cannot issue UUIDv4 token: " + err.Error())
+				}
+				tokenFile, err := os.Create(os.Getenv("FSMQ_TOKEN_POOL_PATH") + "/token-" + tokenUUID.String())
+				if err != nil {
+					log.Fatalln("Cannot create token file: " + err.Error())
+				}
 
-                defer tokenFile.Close()
+				defer tokenFile.Close()
 
-                _, err = tokenFile.WriteString("{\"producer\":" + strconv.FormatBool(element.Producer) +"}")
-                if err != nil {
-                    log.Fatalln("Cannot write token to file:" + err.Error())
-                }
-                fmt.Fprint(res, tokenUUID.String() + "\n")
-                break
-            }
-        }
-        if !authSucceeded {
-            res.WriteHeader(403)
-            fmt.Fprint(res, "Authentication failed")
-        }
-    }
+				_, err = tokenFile.WriteString("{\"producer\":" + strconv.FormatBool(element.Producer) + "}")
+				if err != nil {
+					log.Fatalln("Cannot write token to file:" + err.Error())
+				}
+				fmt.Fprint(res, tokenUUID.String()+"\n")
+				break
+			}
+		}
+		if !authSucceeded {
+			res.WriteHeader(403)
+			fmt.Fprint(res, "Authentication failed")
+		}
+	}
 }
